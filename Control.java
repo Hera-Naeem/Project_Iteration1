a78a9021e22351ae7611208872fc837a22e0efeb
jcsicat <74794260+jcsicat@users.noreply.github.com>
2022-11-03 13:04:43 -0600

    Update Control.java

--- Control.java
+++ Control.java
@@ -1,32 +1,40 @@
 package com.diy.control;
 
+import com.diy.hardware.DoItYourselfStation;
 import com.jimmyselectronics.Item;
 import com.jimmyselectronics.opeechee.Card;
 
 public class Control {
-
-	public void addItem(int choice, Item item) {
-		if(choice == 1) addItemScan();
-		if(choice == 2) addItemText();
-		if(choice == 3) addItemBrowse();
+	
+	
+	
+	
+	
+	public void addItem(int choice, Item item, DoItYourselfStation station) {
+		if(choice == 1) 
+			addItemScan(item, station);
+		if(choice == 2) 
+			addItemText();
+		if(choice == 3) 
+			addItemBrowse();
 		else System.out.printf("Invalid choice");
 	}
 	
-	void addItemScan() {
+	void addItemScan(Item item, DoItYourselfStation station) {
 		
 	}
 	
 	void addItemText() {}
 	void addItemBrowse() {}
 	
-	public void pay(int choice, Card card) {
-		if(choice == 1) payCredit();
+	public void pay(int choice, Card card, DoItYourselfStation station) {
+		if(choice == 1) payCredit(card, station);
 		if(choice == 2) payCash();
 		if(choice == 3) payCrypto();
 		else System.out.printf("Invalid choice");
 	}
 	
-	void payCredit() {
+	void payCredit(Card card, DoItYourselfStation station) {
 		
 	}
 	

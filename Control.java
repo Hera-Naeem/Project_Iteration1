1e32783974c3f7b871c22d4abb8aba429a73feb9
jcsicat <74794260+jcsicat@users.noreply.github.com>
2022-11-03 12:26:23 -0600

    Update Control.java

--- Control.java
+++ Control.java
@@ -1,21 +1,33 @@
 package com.diy.control;
 
+
 public class Control {
-	
-  public void addItem() {
-		
+
+	public void addItem(int choice) {
+		if(choice == 1) addItemScan();
+		if(choice == 2) addItemText();
+		if(choice == 3) addItemBrowse();
+		else System.out.printf("Invalid choice");
 	}
 	
 	void addItemScan() {
 		
 	}
 	
+	void addItemText() {}
+	void addItemBrowse() {}
 	
-	public void pay() {
-		
+	public void pay(int choice) {
+		if(choice == 1) payCredit();
+		if(choice == 2) payCash();
+		if(choice == 2) payCrypto();
+		else System.out.printf("Invalid choice");
 	}
 	
 	void payCredit() {
 		
 	}
+	
+	void payCash() {}
+	void payCrypto() {}
 }

78cd2ce0a5848f83d652cd091c425356e6bb36b2
Catalinag15 <115581750+Catalinag15@users.noreply.github.com>
2022-11-03 18:05:17 -0600

    Update Control.java

--- Control.java
+++ Control.java
@@ -1,7 +1,6 @@
 package com.diy.control;
 
-import java.io.IOException;
-import java.util.Scanner;
+//import java.io.IOException;
 
 import com.diy.hardware.DoItYourselfStation;
 import com.jimmyselectronics.Item;
@@ -11,17 +10,6 @@ import com.jimmyselectronics.opeechee.Card;
 import ca.ucalgary.seng300.simulation.NullPointerSimulationException;
 
 public class Control {
-	
-	private double totalDue;
-	
-	/**
-	 * @param choice
-	 * 	which of 3 ways will we be adding the item
-	 * @param item
-	 *            what item are we trying to add
-	 * @param station
-	 *            at what station is this occurring
-	 */
 	
 	public void addItem(int choice, Item item, DoItYourselfStation station) {
 		if(choice == 1) 
@@ -33,8 +21,21 @@ public class Control {
 		else System.out.printf("Invalid choice"); // default choice choice be addItemScan
 
 		// actually adding the item to the bagging area
-		
 		station.baggingArea.add(item);
+		
+		
+		
+		/////////////////// what we discussed in tutotial
+
+		// instantiate listener and station
+		ControlBarcodeScannerListener ourListener = new ControlBarcodeScannerListener(); 
+		DoItYourselfStation ourStation = new DoItYourselfStation();
+		// register our listener to our scanner 
+		ourStation.scanner.register(ourListener);
+		
+
+
+		
 
 	}
 	
@@ -59,52 +60,32 @@ public class Control {
 		// Continue trying to scan the item until it works
 		boolean successfulScan = false;
 		while (successfulScan == false) {
+			System.out.println("scan failed please try again");
 			station.scanner.scan(item);
 		}	
+		
+		
+		System.out.println("Please place your item in the bagging area");
+		
+				
 	}
+	
+	
+	
+	
 	
 	void addItemText() {}
 	void addItemBrowse() {}
 	
-/*-------------------------------------------------------------------------------------------------------
-	
-	/**
-	 * 	which of 3 ways will we be adding the item
-	 * @param card
-	 *            what card are we using
-	 * @param station
-	 *            at what station is this occurring
-	 * @throws IOException 
-	 */
-	
-	public void pay(Card card, DoItYourselfStation station) throws IOException {
-		Scanner input = new Scanner(System.in);
-		// Prompts user to pay until totalDue is 0
-		// only using System I/O at the moment
-		while (totalDue > 0) {
-			System.out.printf("Total amount due: %d\n", totalDue);
-			System.out.println("1. Pay with Credit");
-			System.out.println("2. Pay with Cash");
-			System.out.println("3. Pay with Crypto");
-			System.out.printf("Choose your payment method: ");
-			int choice = input.nextInt();
-			if(choice == 1) payCredit(card, station);
-			if(choice == 2) payCash();
-			if(choice == 3) payCrypto();
-			else System.out.printf("Invalid choice");
-		}
-		input.close();
+	public void pay(int choice, Card card, DoItYourselfStation station) {
+		if(choice == 1) payCredit(card, station);
+		if(choice == 2) payCash();
+		if(choice == 3) payCrypto();
+		else System.out.printf("Invalid choice");
 	}
 	
-	/*
-	* payCredit() is the method we are fully developing
-	*/
-	void payCredit(Card card, DoItYourselfStation station) throws IOException {
-		Scanner input = new Scanner(System.in);
-		System.out.printf("Enter your pin: ");
-		String pin = input.toString();
-		station.cardReader.insert(card, pin);
-		input.close();
+	void payCredit(Card card, DoItYourselfStation station) {
+		
 	}
 	
 	void payCash() {}

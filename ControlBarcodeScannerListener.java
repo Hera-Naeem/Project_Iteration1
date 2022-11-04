48b2ce55c64a529171c8020ff7b053bbc465caa2
Catalinag15 <115581750+Catalinag15@users.noreply.github.com>
2022-11-04 11:21:27 -0600

    Update ControlBarcodeScannerListener.java

--- ControlBarcodeScannerListener.java
+++ ControlBarcodeScannerListener.java
@@ -6,13 +6,39 @@ import com.jimmyselectronics.necchi.*;
 public class ControlBarcodeScannerListener implements BarcodeScannerListener {
 
 
+	// additions by catalina friday morning base on profs code ///////////
+	
+	// Here, we will record the device on which an event occurs.
+	public AbstractDevice<? extends AbstractDeviceListener> device = null;
+	
+	//Here, we will record the barcode that has been scanned.
+	public Barcode barcode = null;
+	
+	//This is the name of this listener
+	public String name;
+
+	
+	//Basic constructor.
+	//@param name- The name to use for this listener.
+	public ControlBarcodeScannerListener(String name) {
+		this.name = name;
+	}
+	
+////////////////////////////////////////////////////////////////////////////
+	
 	/*
 	 * Here lies our trouble in sending the necessary information for Control
 	 */
+	
 	@Override
 	public void barcodeScanned(BarcodeScanner barcodeScanner, Barcode barcode) {
 		//used to signal Control the barcode that was scanned
-		// check SampleBarcodeScannerListener from his code
+		
+		//Copy paste from the profs sampleBarcodeScannerListener class
+		this.device = barcodeScanner;
+		this.barcode = barcode;
+		System.out.println(name + ": A barcode has been scanned: " + barcode.toString());
+		
 	}
 	
 	@Override
